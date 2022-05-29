ee7ecc57743118b79b5149c366cab33565124e3e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-29 14:38:43 -0300

    Update Ok

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 /*Preview products*/
-let previewContainer = document.querySelector('.products-preview');
-let previewBox = previewContainer.querySelectorAll('.preview');
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.products-container .product').forEach(product => {
-    product.onClick = () => {
-        let name = product.getAttribute('data-name');
-    };
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
 });
 
-
-
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
 
 
 
